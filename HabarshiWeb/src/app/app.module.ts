39c4cfdcec72436acbab1815f0a5c447babf9942
alekspo <aleksey.popryaduhin@thedaedals.com>
2017-04-22 17:17:38 +0300

    Texting.

--- HabarshiWeb/src/app/app.module.ts
+++ HabarshiWeb/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TextingComponent } from './texting/texting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextingComponent
   ],
   imports: [
     BrowserModule,
